47ed133b2892762736d218da3540a3ba83750ff3
cytocracy <theop.parker@gmail.com>
2024-01-18 10:28:00 -0800

    ml 1/18

--- bacon/lab.py
+++ bacon/lab.py
@@ -1,10 +1,24 @@
 import json
 
 def make_actor_dictionary(data):
-    raise NotImplementedError("Implement me!")
+    actors = {}
+    for actorPair in data:
+        actor0 = actorPair[0]
+        actor1 = actorPair[1]
+        if actor0 in actors:
+            actors[actor0].append(actor1)
+        else:
+            actors[actor0] = [actor1]
+
+        if actor1 in actors:
+            actors[actor1].append(actor0)
+        else:
+            actors[actor1] = [actor0]
+    return actors
 
 def did_x_and_y_act_together(data, actor_id_1, actor_id_2):
-    raise NotImplementedError("Implement me!")
+    actors = make_actor_dictionary(data)
+    return actor_id_2 in actors[actor_id_1] or actor_id_1 in actors[actor_id_2]
 
 def get_actors_with_bacon_number(data, n):
     raise NotImplementedError("Implement me!")
@@ -25,6 +39,6 @@ if __name__ == '__main__':
     # additional code here will be run only when lab.py is invoked directly
     # (not when imported from test.py), so this is a good place to put code
     # used, for example, to generate the results for the online questions.
-    with open("resources/movieToID.json") as f:
+    with open("resources/tiny.json") as f:
         data = json.load(f)
     print(data)
