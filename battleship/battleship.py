21c3707df12292a54b31f0adb3c0e6ee8433d0d0
cytocracy <theop.parker@gmail.com>
2024-01-21 19:59:27 -0800

    battleship done

--- battleship/battleship.py
+++ battleship/battleship.py
@@ -8,12 +8,6 @@ class Battleship:
         self.ships = self.getShipDictionary()
 
     def getShipDictionary(self):
-        '''
-        Returns a dictionary mapping ship types to the
-        coordinates they are placed in on self.board.
-        Make sure "~" is not added to the dictionary since 
-        it's not a ship!
-        '''
         locDic = {}
 
         board = self.board
@@ -31,11 +25,6 @@ class Battleship:
         return locDic
 
     def getNumShipsRemaining(self):
-        '''
-        Returns the number of ships that are still afloat 
-        (i.e. ships that still have >0 coordinates remaining
-        in self.ships)
-        '''
         locDic = self.ships
         # print(locDic)
         count = 0
@@ -46,23 +35,9 @@ class Battleship:
         return count
 
     def getLocationsFiredAt(self):
-        '''
-        Returns the list of locations that have already been
-        fired at.
-        '''
-    
         return self.firedAt
 
     def makeMove(self, coord):
-        '''
-        Returns True if the shot hit a ship, and False otherwise.
-        If a shot was fired at a place it has been fired at previously,
-        should also return False
-
-        Updates self.firedAt when a new location has been targeted.
-        Updates self.ships to remove a coordinate of a ship 
-        if one has been hit.
-        '''
 
         self.firedAt.append(coord)
         if self.board[coord[0]][coord[1]] != "~":
@@ -71,18 +46,6 @@ class Battleship:
         return False
 
     def makeHiddenBoard(self):
-        '''
-        Returns the "visible" version of the board based on these
-        rules:
-        * A location that hasn't been fired on should be water ("~")
-        * A location that has been fired on and is a miss (no ship)
-          should be blank (" ")
-        * A location that has been fired on but is not a complete
-          wrecked ship should be an asterisk ("*")
-        * And lastly, a location that has been fired on where the ship
-          is completely sunk should be the letter corresponding to that 
-          ship (either "C", "B", "D", "S", or "P")
-        '''
         returnBoard = deepcopy(self.board)
         for r in range(len(self.board)):
             for c in range(len(self.board[0])):
@@ -96,15 +59,7 @@ class Battleship:
                     else: returnBoard[r][c] = self.board[r][c]
         return returnBoard
 
-                
-
     def printBoard(self, hidden): 
-        '''
-        Prints a version of the board to the terminal.
-        Parameter hidden is a boolean, set to true if ships that 
-        have not been hit on the board yet should remain hidden,
-        false otherwise
-        '''
         if hidden:
             boardToUse = self.makeHiddenBoard()
         else:
@@ -116,6 +71,3 @@ class Battleship:
             for c in range(len(boardToUse[0])):
                 print(boardToUse[r][c], end="")
             print()
-    
-    
-
--- battleship/battleshipProgram.py
+++ battleship/battleshipProgram.py
@@ -1,22 +1,39 @@
 from battleship import Battleship
+import sys
 
-game = Battleship()
+game = True
 
-print("Hello")
-print("You have " + str(game.getNumShipsRemaining()) + " ships remaining")
-game.printBoard(False)
+print("\n\nHELLO AND WELCOME TO BATTLESHIP WOOO\n\n")
 
-while game.getNumShipsRemaining() > 0:
-    print("Where would you like to guess?")
-    row = int(input("Row: "))
-    col = int(input("Col: "))
-    coord = (row, col)
-    if game.makeMove(coord):
-        print("Hit!")
-    else: print("Miss.")
-    print("You have " + str(game.getNumShipsRemaining()) + " ships remaining.")
-    print("---------------------------------------")
-    game.printBoard(False)
+while game:
+    game = Battleship()
+
+    print("You have " + str(game.getNumShipsRemaining()) + " ships remaining")
+    print("---------------------------------------\n")
+
+    game.printBoard(True)
+
+    while game.getNumShipsRemaining() > 0:
+        print("Where would you like to guess?")
+        try:
+            row = int(input("Row: "))
+            col = int(input("Col: "))
+        except ValueError:
+            print("U did a silly and fat fingered the enter button ig")
+            continue
+        coord = (row, col)
+        if game.makeMove(coord):
+            print("Hit!")
+        else: print("Miss.")
+        print("You have " + str(game.getNumShipsRemaining()) + " ships remaining.")
+        print("---------------------------------------\n")
+        game.printBoard(True)
+
+    print("You won!")
+    print("Would you like to play again?")
+    response = input("Y/N: ")
+    if response.lower() == "n":
+        game = False
 
 
-print('yippee')
+print('yippee bye bye')
